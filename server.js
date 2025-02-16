a589fd96bcf9cc22faefeac2a876ca5a05aed8ff
Shree Varshana <varshanaoviya@gmail.com>
2025-02-16 10:44:39 +0530

    chapter 1

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 console.log("Hello Bala")
-console.log(global)
+console.log(global) // It is the object in the Node.js and in JS the object is named as "Window". "Global " is small when compared to Window"
 
 var os = require("os")
 var path = require("path")
@@ -14,3 +14,17 @@ console.log("-----");
 console.log(path.dirname(__filename))
 console.log(path.basename(__filename))
 console.log(path.extname(__filename))
+console.log("-----")
+console.log(path.parse(__filename)) // Give all the object details of the file. Commonly used.
+
+console.log("------------------------------------------------")
+
+var math = require('./math.js') // Also can be done as destructures like "const {add, mul, sub, div} = require('./math)"
+
+console.log(math.add(3, 3)) //Then the call be like "add(3,3)"
+console.log(math.sub(3, 3))
+console.log(math.mul(3, 3))
+console.log(math.div(3, 3))
+
+
+// Instead of .js if we put the .mjs the ES6 can be used efficiently like the "import os from 'os' " this explicitly imports the ES6 into the Javascript
